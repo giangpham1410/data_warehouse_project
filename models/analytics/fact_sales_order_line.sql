6151783aeeae1f3366814f290e8dc253d8295b7c
giangpham1410 <trucgiang.pn@gmail.com>
2025-05-24 17:48:12 +0700

    CTE cast type

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -17,8 +17,18 @@ WITH
     FROM fact_sales_order_line__source
 )
 
+, fact_sales_order_line__cast_type AS (
+    SELECT
+      CAST(sales_order_line_key AS INTEGER) AS sales_order_line_key
+      , CAST(product_key AS INTEGER) AS product_key
+      , CAST(customer_key AS INTEGER) AS customer_key
+      , CAST(quantity AS INTEGER) AS quantity
+      , CAST(unit_price AS NUMERIC) AS unit_price
+    FROM fact_sales_order_line__rename_column
+)
+
 SELECT *
-FROM fact_sales_order_line__rename_column
+FROM fact_sales_order_line__cast_type
 
 
 /*
