87883cc428cf3ec874d927aa66d818d77b97b8af
giangpham1410 <trucgiang.pn@gmail.com>
2025-05-24 17:43:15 +0700

    add customer_id

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,11 +1,25 @@
 WITH
   fact_sales_order_line__source AS (
-    SELECT *
+    SELECT
+      sales_line.*
+      , sales_header.customer_id
     FROM `vit-lam-data.wide_world_importers.sales__order_lines` sales_line
-      LEFT JOIN `vit-lam-data.wide_world_importers.sales__order_lines` sales_header ON sales_line.order_id = sales_header.order_id
+      LEFT JOIN `vit-lam-data.wide_world_importers.sales__orders` sales_header ON sales_line.order_id = sales_header.order_id
 )
+/*
+, fact_sales_order_line__rename_column AS (
+    SELECT
+      order_line_id AS sales_order_line_key
+      , stock_item_id AS product_key
+      , customer_id AS customer_key
+      , quantity
+      , unit_price
+    FROM fact_sales_order_line__source
+)
+*/
 SELECT *
 FROM fact_sales_order_line__source
+
 
 /*
 , fact_sales_order_line__rename_column AS (
