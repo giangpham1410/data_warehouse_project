dac2a8d1547638e15d1b6c253cd6669c1cc9a962
giangpham1410 <trucgiang.pn@gmail.com>
2025-05-24 16:52:19 +0700

    CTE dim_customer__source

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,8 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+WITH
+  dim_customer__source AS (
+    SELECT *
+    FROM `vit-lam-data.wide_world_importers.sales__customers`
+)
+
+SELECT *
+FROM dim_customer__source
