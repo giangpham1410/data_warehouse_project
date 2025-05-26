13f5e237e54c5255c0154550dcf6a0a06dd5a4ee
giangpham1410 <trucgiang.pn@gmail.com>
2025-05-26 16:13:42 +0700

    add supplier_key

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -9,6 +9,7 @@ WITH
       stock_item_id AS product_key
       , stock_item_name AS product_name
       , brand AS brand_name
+      , supplier_id AS supplier_key
     FROM dim_product__source
 )
 
@@ -17,11 +18,16 @@ WITH
       CAST(product_key AS INTEGER) AS product_key
       , CAST(product_name AS STRING) AS product_name
       , CAST(brand_name AS STRING) AS brand_name
+      , CAST(supplier_key AS INTEGER) AS supplier_key
     FROM dim_product__rename_column
 )
 
 SELECT
-  product_key
-  , product_name
-  , brand_name
-FROM dim_product__cast_type
+  product.product_key
+  , product.product_name
+  , product.brand_name
+  , product.supplier_key
+  , supplier.supplier_name
+FROM dim_product__cast_type product
+  LEFT JOIN `data-warehouse-project-459212.wide_world_importers_dwh.dim_supplier` supplier
+    ON product.supplier_key = supplier.supplier_key
