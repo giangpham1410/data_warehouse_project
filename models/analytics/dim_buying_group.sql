15e75aab58c1ebb56ab63482b8b7db97fa8fe931
giangpham1410 <trucgiang.pn@gmail.com>
2025-05-26 20:11:16 +0700

    CTE rename column

--- models/analytics/dim_buying_group.sql
+++ models/analytics/dim_buying_group.sql
@@ -4,5 +4,12 @@ WITH
     FROM `vit-lam-data.wide_world_importers.sales__buying_groups`
 )
 
+, dim_buying_group__rename_column AS (
+  SELECT
+    buying_group_id AS buying_group_key
+    , buying_group_name
+  FROM dim_buying_group__source
+)
+
 SELECT *
-FROM dim_buying_group__source
+FROM dim_buying_group__rename_column
