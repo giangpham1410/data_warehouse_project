15eb3ed8b5c0b77504aba2b74d5f4e0dfef98ea8
giangpham1410 <trucgiang.pn@gmail.com>
2025-05-26 19:55:10 +0700

    add customer_category_key and buying_group_key

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -8,6 +8,8 @@ WITH
   SELECT
     customer_id AS customer_key
     , customer_name
+    , customer_category_id AS customer_category_key
+    , buying_group_id AS buying_group_key
   FROM dim_customer__source
 )
 
@@ -15,10 +17,14 @@ WITH
   SELECT
     CAST(customer_key AS INTEGER) AS customer_key
     , CAST(customer_name AS STRING) AS customer_name
+    , CAST(customer_category_key AS INTEGER) AS customer_category_key
+    , CAST(buying_group_key AS INTEGER) AS buying_group_key
   FROM dim_customer__rename_column
 )
 
 SELECT
   customer_key
   , customer_name
+  , customer_category_key
+  , buying_group_key
 FROM dim_customer__cast_type
