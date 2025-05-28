341bee48ea0f252ac20e2a87dd8e57bf18e2e5ac
giangpham1410 <trucgiang.pn@gmail.com>
2025-05-28 20:49:10 +0700

    add picked_by_person_key

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -35,6 +35,7 @@ SELECT
   , fact_so_line.sales_order_key
   , fact_so_header.customer_key 
   , fact_so_line.product_key
+  , fact_so_header.picked_by_person_key
   , fact_so_line.quantity
   , fact_so_line.unit_price
   , fact_so_line.gross_mount
