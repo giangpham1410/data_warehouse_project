71a744bb93123663877f8499c8f8c4f79c9307dc
giangpham1410 <trucgiang.pn@gmail.com>
2025-05-29 19:11:59 +0700

    add undefined_record = 0

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,23 +1,37 @@
 WITH
   dim_person__source AS (
-    SELECT 
-      *
+    SELECT *
     FROM `vit-lam-data.wide_world_importers.application__people`
 )
+
 , dim_person__rename_column AS (
-    SELECT
-      person_id AS person_key
-      , full_name
-    FROM dim_person__source
+  SELECT
+    person_id AS person_key,
+    full_name
+  FROM dim_person__source 
 )
+
 , dim_person__cast_type AS (
+  SELECT
+    CAST(person_key AS INTEGER) AS person_key
+    , CAST(full_name AS STRING) AS full_name
+  FROM dim_person__rename_column
+)
+
+, dim_person__add_undefined_record AS (
     SELECT
-      CAST(person_key AS INTEGER) AS person_key
-      , CAST(full_name AS STRING) AS full_name
-    FROM dim_person__rename_column
+      person_key
+      , full_name
+    FROM dim_person__cast_type
+
+    UNION ALL
+
+    SELECT 
+      0 AS person_key
+      , 'Undefined' AS full_name
 )
 
 SELECT
   person_key
   , full_name
-FROM dim_person__cast_type
+FROM dim_person__add_undefined_record
