4649cb4591df7fce191e0e83ba49a67f5176024c
giangpham1410 <trucgiang.pn@gmail.com>
2025-05-31 15:58:13 +0700

    CTE handle null

--- models/staging/stg_fact_sales_order.sql
+++ models/staging/stg_fact_sales_order.sql
@@ -21,8 +21,16 @@ WITH
     FROM fact_sales_order__rename_column
 )
 
+, fact_sales_order__handle_null AS (
+    SELECT
+      sales_order_key
+      , COALESCE(customer_key, 0) AS customer_key
+      , COALESCE(picked_by_person_key, 0) AS picked_by_person_key
+    FROM fact_sales_order__cast_type
+)
+
 SELECT
   sales_order_key
   , customer_key
-  , COALESCE(picked_by_person_key, 0) AS picked_by_person_key
-FROM fact_sales_order__cast_type
+  , picked_by_person_key
+FROM fact_sales_order__handle_null
