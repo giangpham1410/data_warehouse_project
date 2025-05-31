e01633871bf6eae27767cc9c6fa02375f61f66b5
giangpham1410 <trucgiang.pn@gmail.com>
2025-05-31 16:33:57 +0700

    add order_date

--- models/staging/stg_fact_sales_order.sql
+++ models/staging/stg_fact_sales_order.sql
@@ -8,6 +8,7 @@ WITH
 , fact_sales_order__rename_column AS (
     SELECT
       order_id  AS sales_order_key
+      , order_date
       , customer_id AS customer_key
       , picked_by_person_id AS picked_by_person_key
     FROM fact_sales_order__source
@@ -16,6 +17,7 @@ WITH
 , fact_sales_order__cast_type AS (
     SELECT
       CAST(sales_order_key AS INTEGER) AS sales_order_key
+      , CAST(order_date AS DATE) AS order_date
       , CAST(customer_key AS INTEGER) AS customer_key
       , CAST(picked_by_person_key AS INTEGER) AS picked_by_person_key
     FROM fact_sales_order__rename_column
@@ -24,6 +26,7 @@ WITH
 , fact_sales_order__handle_null AS (
     SELECT
       sales_order_key
+      , order_date
       , COALESCE(customer_key, 0) AS customer_key
       , COALESCE(picked_by_person_key, 0) AS picked_by_person_key
     FROM fact_sales_order__cast_type
@@ -31,6 +34,7 @@ WITH
 
 SELECT
   sales_order_key
+  , order_date
   , customer_key
   , picked_by_person_key
 FROM fact_sales_order__handle_null
