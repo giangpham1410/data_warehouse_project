93a8809316f54083816bd7d864afe8d660747840
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-03 20:02:58 +0700

    CTE rename_column

---
+++ models/staging/stg_dim_state_province.sql
@@ -0,0 +1,16 @@
+WITH
+  dim_state_province__source AS (
+    SELECT *
+    FROM `vit-lam-data.wide_world_importers.application__state_provinces`
+)
+
+, dim_state_province__rename_column AS (
+    SELECT
+      state_province_id AS state_province_key
+      , state_province_name
+      , state_province_code
+    FROM dim_state_province__source
+)
+
+SELECT *
+FROM dim_state_province__rename_code
