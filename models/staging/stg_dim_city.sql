2225298fedd81d765bd4cb763652da8a5b1cff9d
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-03 20:30:56 +0700

    CTE add undefined record

--- models/staging/stg_dim_city.sql
+++ models/staging/stg_dim_city.sql
@@ -13,5 +13,38 @@ WITH
 
 )
 
-SELECT *
-FROM dim_city__rename_column
+, dim_city__cast_type AS (
+    SELECT
+      CAST(city_key AS INTEGER) AS city_key 
+      , CAST(city_name AS STRING) AS city_name 
+      , CAST(state_province_key AS INTEGER) AS state_province_key 
+    FROm dim_city__rename_column
+)
+
+, dim_city__add_undefined_record AS (
+    SELECT
+      city_key
+      , city_name
+      , state_province_key
+    FROM dim_city__cast_type
+
+    UNION ALL
+
+    SELECT
+      0 AS city_key
+      , 'Undefined' AS city_name
+      , 0 AS state_province_key
+
+    UNION ALL
+
+    SELECT
+      -1 AS city_key
+      , 'Invalid' AS city_name
+      , -1 AS state_province_key
+)
+
+SELECT
+  city_key
+  , city_name
+  , state_province_key
+FROM dim_city__add_undefined_record
