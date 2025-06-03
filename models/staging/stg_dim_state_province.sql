cd0ff99e8ce68f6e6f7cef4b7e88eb2125de5f0c
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-03 20:16:48 +0700

    CTE add_undefined_record

--- models/staging/stg_dim_state_province.sql
+++ models/staging/stg_dim_state_province.sql
@@ -20,5 +20,30 @@ WITH
     FROM dim_state_province__rename_column
 )
 
-SELECT *
-FROM dim_state_province__cast_type
+, dim_state_province__add_undefined_record AS (
+    SELECT
+      state_province_key
+      , state_province_name
+      , state_province_code
+    FROM dim_state_province__cast_type
+
+    UNION ALL
+
+    SELECT
+      0 AS state_province_key
+      , 'Undefined' AS state_province_name
+      , 'Undefined' AS state_province_code
+
+    UNION ALL
+
+    SELECT
+      -1 AS state_province_key
+      , 'Invalid' AS state_province_name
+      , 'Invalid' AS state_province_code
+)
+
+SELECT
+  state_province_key
+  , state_province_name
+  , state_province_code
+FROM dim_state_province__add_undefined_record
