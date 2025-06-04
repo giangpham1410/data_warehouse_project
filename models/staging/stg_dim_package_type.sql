32934f68ed6bb836c93c94c59877ff87d887aaf3
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-04 22:16:44 +0700

    CTE add undefined record

--- models/staging/stg_dim_package_type.sql
+++ models/staging/stg_dim_package_type.sql
@@ -13,10 +13,31 @@ WITH
 
 , dim_package_type__cast_type AS (
     SELECT
-      CAST(package_type_key AS INTEGER) AS package_type_key 
-      , CAST(package_type_name AS STRING) AS package_type_name 
+      CAST(package_type_key AS INTEGER) AS package_type_key
+      , CAST(package_type_name AS STRING) AS package_type_name
     FROM dim_package_type__rename_column
 )
 
-SELECT *
-FROM dim_package_type__cast_type
+, dim_package_type__add_undefined_record AS (
+    SELECT
+      package_type_key
+      , package_type_name
+    FROM dim_package_type__cast_type
+
+    UNION ALL
+
+    SELECT
+      0 AS package_type_key
+      , 'Undefined' AS package_type_name
+
+    UNION ALL
+
+    SELECT
+      -1 AS package_type_key
+      , 'Invalid' AS package_type_name
+)
+
+SELECT
+  package_type_key
+  , package_type_name
+FROM dim_package_type__add_undefined_record
