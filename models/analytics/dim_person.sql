3945d3cd15f81a84bed9eae5dc5c4280f7a53609
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-05 19:04:15 +0700

    add preferred_name

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -6,8 +6,9 @@ WITH
 
 , dim_person__rename_column AS (
   SELECT
-    person_id AS person_key,
-    full_name
+    person_id AS person_key
+    , full_name
+    , preferred_name
   FROM dim_person__source 
 )
 
@@ -15,6 +16,7 @@ WITH
   SELECT
     CAST(person_key AS INTEGER) AS person_key
     , CAST(full_name AS STRING) AS full_name
+    , CAST(preferred_name AS STRING) AS preferred_name
   FROM dim_person__rename_column
 )
 
@@ -22,6 +24,7 @@ WITH
     SELECT
       person_key
       , full_name
+      , preferred_name
     FROM dim_person__cast_type
 
     UNION ALL
@@ -29,15 +32,18 @@ WITH
     SELECT 
       0 AS person_key
       , 'Undefined' AS full_name
+      , 'Undefined' AS preferred_name
 
     UNION ALL
 
     SELECT 
       -1 AS person_key
       , 'Invalid' AS full_name
+      , 'Invalid' AS preferred_name
 )
 
 SELECT
   person_key
   , full_name
+  , preferred_name
 FROM dim_person__add_undefined_record
