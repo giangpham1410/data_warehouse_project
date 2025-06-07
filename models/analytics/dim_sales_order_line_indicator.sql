d34ae4f1f7f5521c9d6ef113a737bf1c1abfb78c
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-07 20:14:56 +0700

    create table

---
+++ models/analytics/dim_sales_order_line_indicator.sql
@@ -0,0 +1,20 @@
+WITH
+  dim_is_undersupply_backordered AS (
+    SELECT
+      TRUE AS is_undersupply_backordered_boolean
+      , 'Undersupply Backordered' AS is_undersupply_backordered
+    UNION ALL
+    SELECT
+      FALSE AS is_undersupply_backordered_boolean
+      , 'Not Undersupply Backordered' AS is_undersupply_backordered
+)
+
+SELECT
+  CONCAT(dim_is_undersupply_backordered.is_undersupply_backordered_boolean, '~', dim_package_type.package_type_key) AS sales_order_line_indicator_key
+  , dim_is_undersupply_backordered.is_undersupply_backordered_boolean
+  , dim_is_undersupply_backordered.is_undersupply_backordered
+  , dim_package_type.package_type_key
+  , dim_package_type.package_type_name
+FROM dim_is_undersupply_backordered
+CROSS JOIN {{ ref('stg_dim_package_type') }} dim_package_type
+ORDER BY 1,3
