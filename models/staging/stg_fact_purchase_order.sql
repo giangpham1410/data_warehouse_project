24d462a1b0448e06f9891b92978c05f5cc25cf1b
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-10 19:40:28 +0700

    CTE cast type

--- models/staging/stg_fact_purchase_order.sql
+++ models/staging/stg_fact_purchase_order.sql
@@ -17,5 +17,30 @@ WITH
     FROM fact_purchase_order__source
 )
 
+, fact_purchase_order__cast_type AS (
+    SELECT
+      CAST(purchase_order_key AS INTEGER) AS purchase_order_key
+      CAST(is_order_finalized_boolean AS BOOLEAN) AS is_order_finalized_boolean
+      CAST(order_date AS DATE) AS order_date
+      CAST(expected_delivery_date AS DATE) AS expected_delivery_date
+      CAST(supplier_reference AS STRING) AS supplier_reference
+      CAST(supplier_key AS INTEGER) AS supplier_key
+      CAST(delivery_method_key AS INTEGER) AS delivery_method_key
+      CAST(contact_person_key AS INTEGER) AS contact_person_key
+    FROM fact_purchase_order__rename_column
+)
+/*
+, fact_purchase_order__convert_boolean AS (
+    SELECT
+      *
+      , CASE
+          WHEN is_order_finalized_boolean IS TRUE THEN 'Order Finalized'
+          WHEN is_order_finalized_boolean IS FALSE THEN 'Not Order Finalized'
+          WHEN is_order_finalized_boolean IS NULL THEN 'Undefined'
+          ELSE 'Invalid'
+        END AS is_order_finalized
+    FROM fact_purchase_order__rename_column
+)
+*/
 SELECT *
-FROM fact_purchase_order__rename_column
+FROM fact_purchase_order__cast_type
