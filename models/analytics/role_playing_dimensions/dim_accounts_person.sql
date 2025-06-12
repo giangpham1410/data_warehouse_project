595a8e763ac87a4b646ffff038c4d701772a9526
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-12 14:34:04 +0700

    update code

--- models/analytics/role_playing_dimensions/dim_accounts_person.sql
+++ models/analytics/role_playing_dimensions/dim_accounts_person.sql
@@ -2,4 +2,4 @@ SELECT
   person_key AS accounts_person_key
   , full_name AS accounts_person_full_name
 FROM {{ ref('dim_person') }}
-WHERE is_employee <> 'Employee'
+WHERE is_employee <> 'Employee' AND is_salesperson <> 'Not Sales Person'
