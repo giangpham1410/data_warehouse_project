ebdcd3f1ac0cab9057d4d478ed741a996bab0a4a
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-12 13:56:44 +0700

    remove redundant CTE

--- models/analytics/dim_invoice_line_indicator.sql
+++ models/analytics/dim_invoice_line_indicator.sql
@@ -7,29 +7,15 @@ WITH
       'Not Credit Note' AS is_invoice_credit_note
 )
 
-, dim_delivery_method AS (
-  SELECT
-    delivery_method_key
-    , delivery_method_name
-  FROM {{ ref('stg_dim_delivery_method') }}
-)
-
-, dim_package_type AS (
-    SELECT
-      package_type_key
-      , package_type_name
-    FROM {{ ref('stg_dim_package_type') }}
-)
-
 SELECT
   FARM_FINGERPRINT(
-    CONCAT(is_invoice_credit_note, '~', delivery_method_key, '~', package_type_key)
+    CONCAT(dim_is_invoice_credit_note.is_invoice_credit_note, '~', dim_delivery_method.delivery_method_key, '~', dim_package_type.package_type_key)
    ) AS dim_invoice_line_indicator
-  , is_invoice_credit_note
-  , delivery_method_key
-  , delivery_method_name
-  , package_type_key
-  , package_type_name
+  , dim_is_invoice_credit_note.is_invoice_credit_note
+  , dim_delivery_method.delivery_method_key
+  , dim_delivery_method.delivery_method_name
+  , dim_package_type.package_type_key
+  , dim_package_type.package_type_name
 FROM dim_is_invoice_credit_note
-  CROSS JOIN dim_delivery_method
-  CROSS JOIN dim_package_type
+  CROSS JOIN {{ ref('stg_dim_delivery_method') }} dim_delivery_method
+  CROSS JOIN {{ ref('stg_dim_package_type') }} dim_package_type
