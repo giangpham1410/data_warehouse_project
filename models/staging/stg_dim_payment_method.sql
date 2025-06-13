f26a4ca87fa278bf89e6219278d2733105a90878
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-13 11:45:42 +0700

    CTE rename column

---
+++ models/staging/stg_dim_payment_method.sql
@@ -0,0 +1,15 @@
+WITH
+  dim_payment_method__source AS (
+    SELECT *
+    FROM `vit-lam-data.wide_world_importers.application__payment_methods`
+)
+
+, dim_payment_method__rename_column AS (
+    SELECT
+      payment_method_id AS payment_method_key
+      , payment_method_name
+    FROM dim_payment_method__source
+)
+
+SELECT *
+FROM dim_payment_method__rename_column
