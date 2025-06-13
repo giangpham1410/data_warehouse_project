6aa82a9f17be712fc14082e9f021da2495ad33ae
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-13 11:29:30 +0700

    CTE add undefined record

--- models/analytics/dim_transaction_type.sql
+++ models/analytics/dim_transaction_type.sql
@@ -18,6 +18,26 @@ WITH
     FROM dim_transaction_type__rename_column
 )
 
+, dim_transaction_type__add_undefined_record AS (
+    SELECT
+      transaction_type_key
+      , transaction_type_name
+    FROM dim_transaction_type__cast_type
+
+    UNION ALL
+
+    SELECT
+      0 AS transaction_type_key
+      , 'Undefined' AS transaction_type_name
+
+    UNION ALL
+
+    SELECT
+      -1 AS transaction_type_key
+      , 'Invalid' AS transaction_type_name
+)
+
 SELECT
-  *
-FROM dim_transaction_type__cast_type
+  transaction_type_key
+  , transaction_type_name
+FROM dim_transaction_type__add_undefined_record
