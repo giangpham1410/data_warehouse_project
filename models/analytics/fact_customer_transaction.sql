21cae0aa886a5cb4dbc6a8dd37dbf34ad5b1f967
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-13 13:56:06 +0700

    CTE cast_type

--- models/analytics/fact_customer_transaction.sql
+++ models/analytics/fact_customer_transaction.sql
@@ -21,5 +21,46 @@ WITH
     FROM fact_customer_transaction__source
 )
 
-SELECT *
-FROM fact_customer_transaction__rename_column
+, fact_customer_transaction__cast_type AS (
+    SELECT
+      CAST(customer_transaction_key AS INTEGER) AS customer_transaction_key
+      , CAST(is_customer_finalized AS BOOLEAN) AS is_customer_finalized_boolean
+      , CAST(amount_excluding_tax AS NUMERIC) AS amount_excluding_tax
+      , CAST(tax_amount AS NUMERIC) AS tax_amount
+      , CAST(transaction_amount AS NUMERIC) AS transaction_amount
+      , CAST(outstanding_balance AS NUMERIC) AS outstanding_balance
+      , CAST(transaction_date AS DATE) AS transaction_date
+      , CAST(finalization_date AS DATE) AS finalization_date
+      , CAST(customer_key AS INTEGER) AS customer_key
+      , CAST(transaction_type_key AS INTEGER) AS transaction_type_key
+      , CAST(sales_invoice_key AS INTEGER) AS sales_invoice_key
+      , CAST(payment_method_key AS INTEGER) AS payment_method_key
+    FROM fact_customer_transaction__rename_column
+)
+
+, fact_customer_transaction__convert_boolean AS (
+    SELECT
+      *
+      , CASE
+          WHEN is_customer_finalized_boolean IS TRUE THEN 'Customer Finalized'
+          WHEN is_customer_finalized_boolean IS FALSE THEN 'Not Customer Finalized'
+          WHEN is_customer_finalized_boolean IS NULL THEN 'Undefined'
+          ELSE 'Invalid'
+        END AS is_customer_finalized
+    FROM fact_customer_transaction__cast_type
+)
+
+SELECT
+  customer_transaction_key
+  , is_customer_finalized
+  , amount_excluding_tax
+  , tax_amount
+  , transaction_amount
+  , outstanding_balance
+  , transaction_date
+  , finalization_date
+  , customer_key
+  , transaction_type_key
+  , sales_invoice_key
+  , payment_method_key
+FROM fact_customer_transaction__convert_boolean
