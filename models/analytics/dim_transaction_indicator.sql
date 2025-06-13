64700e69d8edada541b71b190850812db8b9021b
giangpham1410 <trucgiang.pn@gmail.com>
2025-06-13 16:01:29 +0700

    create dim_transaction_indicator_key

--- models/analytics/dim_transaction_indicator.sql
+++ models/analytics/dim_transaction_indicator.sql
@@ -5,7 +5,17 @@ WITH
     SELECT 'Not Finalized' AS is_finalized
 )
 
-SELECT *
+SELECT
+  FARM_FINGERPRINT(
+    CONCAT(dim_is_finalized.is_finalized, '~', 
+           dim_payment_method.payment_method_key, '~', 
+           dim_transaction_type.transaction_type_key)
+  ) AS dim_transaction_indicator_key
+  , is_finalized
+  , dim_payment_method.payment_method_key
+  , dim_payment_method.payment_method_name
+  , dim_transaction_type.transaction_type_key
+  , dim_transaction_type.transaction_type_name
 FROM dim_is_finalized
-  CROSS JOIN {{ ref('dim_transaction_type') }}
-  CROSS JOIN {{ ref('stg_dim_payment_method') }}
+  CROSS JOIN {{ ref('dim_transaction_type') }} dim_transaction_type
+  CROSS JOIN {{ ref('stg_dim_payment_method') }} dim_payment_method
